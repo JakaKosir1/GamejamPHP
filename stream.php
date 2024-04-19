c40d6609dfbedb99f2489975ca6cedc7686238ec
Jaka <jaka.kosir05@gmail.com>
2024-04-19 17:27:07 +0200

    stream poprauki

--- stream.php
+++ stream.php
@@ -4,24 +4,33 @@
     <title>Gamejam Stream</title>
     <style>
         body {
-            background-image: "Slike/mainPhoto.png";
-            background-size: cover; /* Adjust as needed */
-            background-position: center; /* Adjust as needed */
-            /* Additional styling for the body */
+            background-image: url("Slike/mainPhoto.png");
+            background-size: cover;
+            background-position: center;
             margin: 0;
             padding: 0;
             font-family: Arial, sans-serif;
         }
+
         /* Optional: Style the embedded Twitch player */
+        .iframe-container {
+            width: 80%;
+            height: 80vh;
+            margin: 10% auto;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Add shadow */
+        }
+
         iframe {
             width: 100%;
-            height: 100vh; /* Adjust the height as needed */
+            height: 100%;
             border: none;
         }
     </style>
 </head>
 <body>
     <!-- Paste the Twitch embed code here -->
-    <iframe src="https://player.twitch.tv/?channel=gamejam2024&parent=gamejam.scv.si" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
+    <div class="iframe-container">
+        <iframe src="https://player.twitch.tv/?channel=gamejam2024&parent=gamejam.scv.si" frameborder="0" allowfullscreen="true" scrolling="no"></iframe>
+    </div>
 </body>
 </html>
