7c5f263752de96db6c126791623741a0c596209b
Jaka <jaka.kosir05@gmail.com>
2024-04-19 17:30:13 +0200

    semal popraukou

--- stream.php
+++ stream.php
@@ -10,13 +10,21 @@
             margin: 0;
             padding: 0;
             font-family: Arial, sans-serif;
+            overflow: hidden; /* Disable scrolling */
+        }
+
+        /* Center the iframe */
+        .center-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh; /* Make the container fill the viewport height */
         }
 
         /* Optional: Style the embedded Twitch player */
         .iframe-container {
             width: 80%;
             height: 80vh;
-            margin: 10% auto;
             box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Add shadow */
         }
 
@@ -28,9 +36,11 @@
     </style>
 </head>
 <body>
-    <!-- Paste the Twitch embed code here -->
-    <div class="iframe-container">
-        <iframe src="https://player.twitch.tv/?channel=gamejam2024&parent=gamejam.scv.si" frameborder="0" allowfullscreen="true" scrolling="no"></iframe>
+    <div class="center-container">
+        <!-- Paste the Twitch embed code here -->
+        <div class="iframe-container">
+            <iframe src="https://player.twitch.tv/?channel=gamejam2024&parent=gamejam.scv.si" frameborder="0" allowfullscreen="true" scrolling="no"></iframe>
+        </div>
     </div>
 </body>
 </html>
